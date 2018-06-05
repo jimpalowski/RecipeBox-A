08d083c89453e23ed9c1e4ee207af458a59fd258
Jimmy McNamara and Jim Palowski <jimmy@>
2018-06-05 11:21:31 -0700

    Add recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  currentTitle: string = 'Recipe Box';
+  recipes: Recipe[] = [
+    new Recipe('Lunch','Eggs, Bread, Milk','Breakfast'),
+    new Recipe('Dinner', 'Ground Beef, Marshmallows, Water', 'Get Creative')
+  ];
 }
---
+++ src/app/models/recipe.model.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var Recipe = /** @class */ (function () {
+    function Recipe(title, ingredients, directions) {
+        this.title = title;
+        this.ingredients = ingredients;
+        this.directions = directions;
+    }
+    return Recipe;
+}());
+exports.Recipe = Recipe;
